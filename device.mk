3d0bd8cc9a48fd0843bc422f20eac2a40cc61cef
dianlujitao <dianlujitao@gmail.com>
2016-05-30 12:50:20 +0800

    che10: Initial commit

---
+++ device.mk
@@ -0,0 +1,27 @@
+#
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$(call inherit-product-if-exists, vendor/huawei/che10/che10-vendor.mk)
+
+# NFC
+PRODUCT_PACKAGES += \
+    NfcNci \
+    nfc_nci.pn54x.default
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
+    $(LOCAL_PATH)/configs/libnfc-nxp.conf:system/etc/libnfc-nxp.conf
+
+$(call inherit-product, device/huawei/msm8916-common/msm8916.mk)
